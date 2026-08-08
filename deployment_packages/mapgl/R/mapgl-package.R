de25f704d666ec6ea245f5f9521be62a83cd0d3e
Akhila Vanga, BDS, MS Clinical Epidemiology <drakhilavanga17@gmail.com>
2026-08-08 13:23:29 -0400

    Add deployment mapgl patch: R/mapgl-package.R

---
+++ deployment_packages/mapgl/R/mapgl-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import rlang
+#' @import sf
+#' @import geojsonsf
+#' @import htmlwidgets
+#' @import htmltools
+#' @importFrom classInt classIntervals
+#' @import terra
+#' @import base64enc
+#' @import grDevices
+## usethis namespace: end
+NULL
